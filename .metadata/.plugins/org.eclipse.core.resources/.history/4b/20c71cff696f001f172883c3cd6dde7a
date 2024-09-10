5e70240edef130f136bba7b84100913ad64a8e67
BaoBao666888 <nguyenquocbaopl321@gmail.com>
2024-09-10 19:54:18 +0700

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4b/20c71cff696f001f172883c3cd6dde7a
@@ -0,0 +1,11 @@
+package com.loginregistration.dao;
+
+import com.loginregistration.model.User;
+
+public interface UserDao{
+	void insert(User user);
+	User get(String username);
+	boolean checkExistEmail(String email);
+	boolean checkExistUsername(String username);
+	boolean checkExistPhone(String phone);
+}
---
+++ baitaptuan3/src/main/java/com/loginregistration/model/Constant.java
@@ -0,0 +1,10 @@
+package com.loginregistration.model;
+
+public class Constant {
+    public static final String SESSION_USERNAME = "username";
+    public static final String COOKIE_REMEMBER = "username";
+    
+    public static class Path { 
+        public static final String REGISTER = "/views/register.jsp";
+    }
+}
